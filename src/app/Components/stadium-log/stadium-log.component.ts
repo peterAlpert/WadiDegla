dfcf1580ddd350b6c3027e2c9e65566f4e790db7
Peter Kameel <peterkameel.93@gmail.com>
2025-06-15 04:26:05 +0300

    ...

--- src/app/Components/stadium-log/stadium-log.component.ts
+++ src/app/Components/stadium-log/stadium-log.component.ts
@@ -154,7 +154,7 @@ export class StadiumLogComponent implements OnInit {
   viewEntryHistory(member: any) {
     console.log(member);
 
-    this._Router.navigate([`/Entry/by-member/${member.memberId}`], { state: { member: member } });
+    this._Router.navigate([`/Entry/by-member/${member.firstEntry.memberId}`], { state: { member: member } });
   }
 
 }
