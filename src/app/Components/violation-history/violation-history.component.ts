ec8265fe1a6621caef0dd86a4f22f5c79e37a919
Peter Kameel <peterkameel.93@gmail.com>
2025-06-17 13:13:22 +0300

    edit in violation

--- src/app/Components/violation-history/violation-history.component.ts
+++ src/app/Components/violation-history/violation-history.component.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { StadiumService } from './../../Services/stadium.service';
 import { Iplayer } from './../../Interfaces/iplayer';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ViolationService } from '../../Services/violation.service';
 import { IViolation } from '../../Interfaces/iviolation';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-violation-history',
@@ -14,8 +17,14 @@ import { IViolation } from '../../Interfaces/iviolation';
 export class ViolationHistoryComponent implements OnInit {
   member: Iplayer = {} as Iplayer;
   violations: IViolation[] = [];
+  allData: any[] = [];
+  filteredData: any[] = [];
 
-  constructor(private router: Router, private violationService: ViolationService) { }
+  constructor(
+    private router: Router,
+    private _StadiumService: StadiumService,
+    private _HttpClient: HttpClient,
+    private violationService: ViolationService) { }
 
   ngOnInit(): void {
     const nav = this.router.getCurrentNavigation();
@@ -27,5 +36,27 @@ export class ViolationHistoryComponent implements OnInit {
         error: (err) => console.error('Error fetching violations:', err)
       });
     }
+
+    this._StadiumService.getAll().subscribe((res: any) => {
+      this.allData = res;
+      this.filteredData = res;
+    });
+
+    this.getAllData();
+  }
+
+  getAllData() {
+    this._HttpClient.get<any[]>(`${environment.baseUrl}/Entry/first-entries`).subscribe((res) => {
+      this.allData = res;
+      this.filteredData = res;
+    });
+  }
+
+  filterByStadium(no: number) {
+    this.filteredData = this.allData.filter(x => x.stadeNo == no);
+  }
+
+  showAll() {
+    this.filteredData = this.allData;
   }
 }
