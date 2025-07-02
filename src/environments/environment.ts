c1cd945658a7b714ee73b30a63a2a0f937ae474a
Peter Kameel <peterkameel.93@gmail.com>
2025-07-02 03:52:53 +0300

    ..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
-export const environment = {
-    baseUrl: 'https://localhost:7183/api'
-};
-
 // export const environment = {
-//     baseUrl: 'https://degla.runasp.net/api'
+//     baseUrl: 'https://localhost:7183/api'
 // };
+
+export const environment = {
+    baseUrl: 'https://degla.runasp.net/api'
+};
 
